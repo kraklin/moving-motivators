c344708ad30efdb3518318563d57eb8c522caf4c
Tomáš  Látal <latal.tomas@gmail.com>
2022-09-27 22:24:56 +0200

    Add vite build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import elm from 'vite-plugin-elm'
 
 export default defineConfig({
+  build: {outDir: "docs" },
+  base: "/moving-motivators/",
   plugins: [elm()]
 })
 
